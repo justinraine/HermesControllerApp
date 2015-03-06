452eed8b861dd85d8c98b53b0cfc0a3ec3dd2eab
Justin Raine <justin@MacBook-Pro.local>
2015-03-05 21:03:35 -0800

    Initial Commit

---
+++ Hermes Controller/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hermes Controller
+//
+//  Created by Justin Raine on 2015-03-05.
+//  Copyright (c) 2015 vitaMotu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
